5136d04f239f5bf4b9b5c1b6a2a5a02abc22c777
danielgonzalez57 <danielsrkt@gmail.com>
2025-03-13 15:21:59 -0400

    13 03 25 segundo commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import './assets/main.css'
+import './index.css'
+import PrimeVue from 'primevue/config';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+      preset: Aura
+  }
+});
+app.component('Button-item', Button);
 
 app.mount('#app')
